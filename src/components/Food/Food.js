d4e0c9a3d1bcee1f7af81339e73249157ca02f5f
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-02 13:38:43 +0600

    event handler added

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -1,11 +1,22 @@
 import React from 'react';
 const Food = (props) => {
     const {menu,price,id} = props.food;
+    const design = {
+        border : '5px solid sandybrown',
+        borderRadius : '10px',
+        margin : '10px',
+        padding : '10px'
+
+    }
+    function addToCart(id){
+        console.log(`Item is added to cart, id number is ${id}`);
+    }
     return (
-        <div>
+        <div style = {design}>
             <h3>Order this Menu : {menu}</h3>
             <small>Price of this Dish : {price}</small>
             <p>It has id : {id}</p>
+            <button onClick ={() => addToCart(id)}>Add to Cart</button>
         </div>
     );
 };
