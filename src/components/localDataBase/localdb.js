f24f3d709790533a9562243b75d62c10894c9982
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-02 16:32:50 +0600

    duplicate item count feature added

--- src/components/localDataBase/localdb.js
+++ src/components/localDataBase/localdb.js
@@ -1,4 +1,15 @@
 const addToDb = (id) => {
-    localStorage.setItem(id , 1 );
+    const quantity = localStorage.getItem(id);
+    if(quantity)
+    {
+        console.log('already exists');
+        const newQuantity = parseInt(quantity) + 1;
+        localStorage.setItem(id , newQuantity);
+    }
+    else
+    {
+        console.log('new item added');
+        localStorage.setItem(id , 1);
+    }
 }
 export {addToDb};
