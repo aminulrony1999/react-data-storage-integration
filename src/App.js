3b2bdcccc30b7f5d9fc2cdeaece894349d412dc8
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-02 20:27:00 +0600

    shopping cart all feature added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Foods from './components/Foods/Foods.js';
 function App() {
   return (
     <div className="App">
-      <h1>Welocme to my Restaurant</h1>
+      <h1>Welcome to my Restaurant</h1>
       <Foods></Foods>
     </div>
   );
